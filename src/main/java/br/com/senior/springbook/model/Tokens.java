a4bbc9705d1e779666462f0400e021020f8817a5
JoaoEmanoelConinch <68817552+JoaoEmanoelConinch@users.noreply.github.com>
2021-12-03 10:30:45 -0300

    Adicionado token

---
+++ src/main/java/br/com/senior/springbook/model/Tokens.java
@@ -0,0 +1,8 @@
+package br.com.senior.springbook.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokens {
+    public static List<String> tokens = new ArrayList<>();
+}
